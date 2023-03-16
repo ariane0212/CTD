bce527e5b9f54ee4888815c68d945158489d2dd3
Ariane Santos <88981609+ariane0212@users.noreply.github.com>
2023-03-16 01:55:14 -0300

    Delete main.js

--- Front-end II/Checkpoint-I/js/main.js
+++
@@ -1,7 +0,0 @@
-const chk = document.getElementById("chk");
-const title = document.getElementById("title");
-const itens = document.getElementById("itens");
-
-chk.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
-});

e61ba08f590a8e2a3a8f51783f16ebefc0e4f6e0
Deborah-Borges <pretinha.deb@gmail.com>
2023-03-17 15:37:43 -0300

    novas atualizações

--- Front-end II/Checkpoint-I/js/script.js
+++ Front-end II/Checkpoint-I/js/script.js
@@ -40,7 +40,7 @@ function validarDescricao() {
     dados.descricao = inputDescricaoRef.value.trim()
     return false
   }
-  dados.mensage1 = inputDescricaoRef.value.trim()
+  dados.descricao = inputDescricaoRef.value.trim()
   mensage1.innerHTML = ''
 
 }
@@ -69,21 +69,52 @@ function validarErro() {
    }
  
  }
+
+
+ function validarImg() {
+  dados.url = inputUrlRef.value.trim()
+  mensage.innerHTML = ''
+ }
+
+const root = document.querySelector('.container')
+function showCard (event) {
+  event.preventDefault()
+  console.log(dados) 
+ const { descricao,titulo, url } = dados
+ const div = document.createElement('div')
+ div.className = 'item'
+ const h1 = document.createElement('h1')
+ h1.innerText = titulo;
+ h1.className = 'item-h2'
+ const img = document.createElement('img')
+ img.src = url
+ img.className = 'img-item'
+ const paragrafo = document.createElement('p')
+ paragrafo.innerText = descricao
+ paragrafo.className = 'item-p'
+ div.appendChild(img) 
+ div.appendChild(h1) 
+ div.appendChild(paragrafo)
+ root.appendChild(div) 
+
+ inputTitulo.value = ''
+ inputUrlRef.value = ''
+ inputDescricaoRef.value = ''
+ 
+ console.log('caiu aqui')
+}
+
+console.log(dados)
  
 inputTitulo.addEventListener('keyup', validarErro)
 inputDescricaoRef.addEventListener('keyup', validarErroDescricao)
 inputUrlRef.addEventListener('keyup', validarErroUrl)
+inputUrlRef.addEventListener('keyup', validarImg)
+
+loginButtonRef.addEventListener('click', showCard)
 
 inputTitulo.addEventListener('keyup', validarTitulo)
 inputDescricaoRef.addEventListener('keyup', validarDescricao)
-
-
-// Objeto que irá informar qual campo ainda possuí erro. Ele será muito importante para termos uma visão geral se o nosso formulário como um todo possuí algum erro ou não
-// var formErrors = {
-//     inputTítulo: true,
-//     userURL: true,
-//     userDescricao: true
-// }
 
 // Botão DARK MODE
 const chk = document.getElementById("chk");
@@ -95,63 +126,63 @@ chk.addEventListener("change", () => {
 });
 
 
-function mudarConteudo() {
-   document.getElementById("item").innerHTML +=
-   `
-       <div class="item">
-           <img src="${item.img}">
-           <h2>${item.titulo}</h2>
-           <p>${item.descricao}</p>
-       </div>
-   `
-}
+// function mudarConteudo() {
+//    document.getElementById("item").innerHTML +=
+//    `
+//        <div class="item">
+//            <img src="${item.img}">
+//            <h2>${item.titulo}</h2>
+//            <p>${item.descricao}</p>
+//        </div>
+//    `
+// }
 
 // parte para add os cards
 const containerRef = document.querySelector('item');
 // containerRef.classList.add('item');
 
-var cards = [
+// var cards = [
 
-    {
-        img:  '<img src="images/01.png">',
-        titulo: '<h2>Bitcoin</h2>',
-        descricao: '<p> Bitcoin é uma criptomoeda livre e descentralizada, um dinheiro eletrônico para transações financeiras ponto a ponto, originalmente descrita em um artigo por um programador ou grupo de programadores sob o pseudônimo Satoshi Nakamoto, publicado em 31 de outubro de 2008 na lista de discussão The Cryptography Mailing.</p>'
+//     {
+//         img:  '<img src="images/01.png">',
+//         titulo: '<h2>Bitcoin</h2>',
+//         descricao: '<p> Bitcoin é uma criptomoeda livre e descentralizada, um dinheiro eletrônico para transações financeiras ponto a ponto, originalmente descrita em um artigo por um programador ou grupo de programadores sob o pseudônimo Satoshi Nakamoto, publicado em 31 de outubro de 2008 na lista de discussão The Cryptography Mailing.</p>'
  
-     },
+//      },
 
-    {
-        img:  '<img src="images/02.png">',
-        titulo: '<h2>Ethereum</h2>',
-        descricao: '<p>Ethereum é uma plataforma descentralizada capaz de executar contratos inteligentes e aplicações descentralizadas usando a tecnologia blockchain. São aplicações que funcionam exatamente como programadas sem qualquer possibilidade de censura, fraude ou interferência de terceiros, isso porque o contrato é imutável.</p>'
+//     {
+//         img:  '<img src="images/02.png">',
+//         titulo: '<h2>Ethereum</h2>',
+//         descricao: '<p>Ethereum é uma plataforma descentralizada capaz de executar contratos inteligentes e aplicações descentralizadas usando a tecnologia blockchain. São aplicações que funcionam exatamente como programadas sem qualquer possibilidade de censura, fraude ou interferência de terceiros, isso porque o contrato é imutável.</p>'
  
-     },
+//      },
 
-     {
-        img:  '<img src="images/03.png">',
-        titulo: '<h2>Tether</h2>',
-        descricao: '<p>Tether é uma criptomoeda hospedada nas blockchains Ethereum e Bitcoin, entre outras. Seus tokens são emitidos pela empresa de Hong Kong Tether Limited, que por sua vez é controlada pelos proprietários da Bitfinex.</p>'
+//      {
+//         img:  '<img src="images/03.png">',
+//         titulo: '<h2>Tether</h2>',
+//         descricao: '<p>Tether é uma criptomoeda hospedada nas blockchains Ethereum e Bitcoin, entre outras. Seus tokens são emitidos pela empresa de Hong Kong Tether Limited, que por sua vez é controlada pelos proprietários da Bitfinex.</p>'
  
-     },
+//      },
 
-     {
-        img:  '<img src="images/04.png">',
-        titulo: '<h2>BNB</h2>',
-        descricao: '<p>A Binance Coin (BNB) é mais uma criptomoeda lançada para uso dentro de uma exchange, assim como o Huobi Token e a Unus Sed Leo. Foi lançada em agosto de 2017 por uma das maiores exchanges (corretora de criptomoedas) do mundo, a Binance.</p>'
+//      {
+//         img:  '<img src="images/04.png">',
+//         titulo: '<h2>BNB</h2>',
+//         descricao: '<p>A Binance Coin (BNB) é mais uma criptomoeda lançada para uso dentro de uma exchange, assim como o Huobi Token e a Unus Sed Leo. Foi lançada em agosto de 2017 por uma das maiores exchanges (corretora de criptomoedas) do mundo, a Binance.</p>'
  
-     },
+//      },
 
-     {
-        img:  '<img src="images/05.png">',
-        titulo: '<h2>Ripple</h2>',
-        descricao: '<p> Ripple é um sistema de liquidação bruta em tempo real, câmbio e rede de remessas desenvolvido pela Ripple. Também chamado de protocolo Ripple Transaction Protocol ou Ripple, é construído com base em um protocolo de Internet aberto distribuído, um livro-razão de consenso e uma moeda nativa chamada XRP.</p>'
+//      {
+//         img:  '<img src="images/05.png">',
+//         titulo: '<h2>Ripple</h2>',
+//         descricao: '<p> Ripple é um sistema de liquidação bruta em tempo real, câmbio e rede de remessas desenvolvido pela Ripple. Também chamado de protocolo Ripple Transaction Protocol ou Ripple, é construído com base em um protocolo de Internet aberto distribuído, um livro-razão de consenso e uma moeda nativa chamada XRP.</p>'
  
-     },
+//      },
      
-     {
-        img:  '<img src="./images/06.png">',
-        titulo: '<h2>Dogecoin</h2>',
-        descricao: '<p>Dogecoin é uma criptomoeda peer-to-peer de código aberto criada inicialmente como uma "moeda piada" em 06 de dezembro de 2013, desenvolvendo rapidamente sua própria comunidade on-line, alcançando uma capitalização de US$ 60 milhões em janeiro de 2014.</p>'
+//      {
+//         img:  '<img src="./images/06.png">',
+//         titulo: '<h2>Dogecoin</h2>',
+//         descricao: '<p>Dogecoin é uma criptomoeda peer-to-peer de código aberto criada inicialmente como uma "moeda piada" em 06 de dezembro de 2013, desenvolvendo rapidamente sua própria comunidade on-line, alcançando uma capitalização de US$ 60 milhões em janeiro de 2014.</p>'
  
-     }
+//      }
 
-];
+// ];
